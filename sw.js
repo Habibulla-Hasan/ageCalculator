4f6ad5ca48f9e0f421cd95dac4856dfc104b59a3
Habibulla-Hasan <habibulla.en@gmail.com>
2026-03-03 14:42:01 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,22 @@
-const CACHE_NAME="age-calculator-v1";
-const urlsToCache=["index.html"];
+const CACHE_NAME = "agedekho-v1";
+const urlsToCache = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
-self.addEventListener("install",event=>{
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache=>cache.addAll(urlsToCache))
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch",event=>{
-  event.respondWith(
-    caches.match(event.request)
-      .then(response=>response || fetch(event.request))
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(response=>response || fetch(e.request))
+  );
+});
+
+self.addEventListener("activate", e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>{
+      return Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)));
+    })
   );
 });
